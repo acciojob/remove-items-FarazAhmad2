72a9ca66c444c67fce4f6bee3eef258adbeae152
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-10 17:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+    let select = document.getElementById("colorSelect");
+    let selectedOption = select.options[select.selectedIndex];
+    
+    if (selectedOption) {
+      select.removeChild(selectedOption);
+    }
+  }
+
+document.getElementsByTagName('input')[0].addEventListener('click', removeColor);
